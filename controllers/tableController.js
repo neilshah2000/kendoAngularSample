a84b77d88b323d0da1f877749a63614af704ebee
Neil Shah <Neil.Shah@grosvenor.local>
2017-07-03 17:23:19 +0100

    init project

---
+++ controllers/tableController.js
@@ -0,0 +1,166 @@
+(function() {
+    'use strict';
+
+    // angular.module("KendoDemos")
+    //     .controller("MyCtrl", function() {
+    //         var vm = this;
+    //         vm.mainGridOptions = {
+    //             dataSource: {
+    //                 type: "odata",
+    //                 transport: {
+    //                     read: "https://demos.telerik.com/kendo-ui/service/Northwind.svc/Employees"
+    //                 },
+    //                 pageSize: 5,
+    //                 serverPaging: true,
+    //                 serverSorting: true
+    //             },
+    //             sortable: true,
+    //             pageable: true,
+    //             dataBound: function() {
+    //                 this.expandRow(this.tbody.find("tr.k-master-row").first());
+    //             },
+    //             columns: [{
+    //                 field: "FirstName",
+    //                 title: "First Name",
+    //                 width: "120px"
+    //             }, {
+    //                 field: "LastName",
+    //                 title: "Last Name",
+    //                 width: "120px"
+    //             }, {
+    //                 field: "Country",
+    //                 width: "120px"
+    //             }, {
+    //                 field: "City",
+    //                 width: "120px"
+    //             }, {
+    //                 field: "Title"
+    //             }]
+    //         };
+
+    //         vm.detailGridOptions = function(dataItem) {
+    //             return {
+    //                 dataSource: {
+    //                     type: "odata",
+    //                     transport: {
+    //                         read: "https://demos.telerik.com/kendo-ui/service/Northwind.svc/Orders"
+    //                     },
+    //                     serverPaging: true,
+    //                     serverSorting: true,
+    //                     serverFiltering: true,
+    //                     pageSize: 5,
+    //                     filter: {
+    //                         field: "EmployeeID",
+    //                         operator: "eq",
+    //                         value: dataItem.EmployeeID
+    //                     }
+    //                 },
+    //                 scrollable: false,
+    //                 sortable: true,
+    //                 pageable: true,
+    //                 columns: [{
+    //                     field: "OrderID",
+    //                     title: "ID",
+    //                     width: "56px"
+    //                 }, {
+    //                     field: "ShipCountry",
+    //                     title: "Ship Country",
+    //                     width: "110px"
+    //                 }, {
+    //                     field: "ShipAddress",
+    //                     title: "Ship Address"
+    //                 }, {
+    //                     field: "ShipName",
+    //                     title: "Ship Name",
+    //                     width: "190px"
+    //                 }]
+    //             };
+    //         };
+    //     })
+
+    angular
+        .module('KendoDemos')
+        .controller('MyCtrl', MyCtrl);
+
+    MyCtrl.$inject = []
+
+    function MyCtrl() {
+        var vm = this;
+        // vm.mainGridOptions = mainGridOptions;
+        vm.detailGridOptions = detailGridOptions;
+
+        vm.mainGridOptions = {
+            dataSource: {
+                type: "odata",
+                transport: {
+                    read: "https://demos.telerik.com/kendo-ui/service/Northwind.svc/Employees"
+                },
+                pageSize: 5,
+                serverPaging: true,
+                serverSorting: true
+            },
+            sortable: true,
+            pageable: true,
+            dataBound: function() {
+                this.expandRow(this.tbody.find("tr.k-master-row").first());
+            },
+            columns: [{
+                field: "FirstName",
+                title: "First Name",
+                width: "120px"
+            }, {
+                field: "LastName",
+                title: "Last Name",
+                width: "120px"
+            }, {
+                field: "Country",
+                width: "120px"
+            }, {
+                field: "City",
+                width: "120px"
+            }, {
+                field: "Title"
+            }]
+        };
+
+        function detailGridOptions(dataItem) {
+            return {
+                dataSource: {
+                    type: "odata",
+                    transport: {
+                        read: "https://demos.telerik.com/kendo-ui/service/Northwind.svc/Orders"
+                    },
+                    serverPaging: true,
+                    serverSorting: true,
+                    serverFiltering: true,
+                    pageSize: 5,
+                    filter: {
+                        field: "EmployeeID",
+                        operator: "eq",
+                        value: dataItem.EmployeeID
+                    }
+                },
+                scrollable: false,
+                sortable: true,
+                pageable: true,
+                columns: [{
+                    field: "OrderID",
+                    title: "ID",
+                    width: "56px"
+                }, {
+                    field: "ShipCountry",
+                    title: "Ship Country",
+                    width: "110px"
+                }, {
+                    field: "ShipAddress",
+                    title: "Ship Address"
+                }, {
+                    field: "ShipName",
+                    title: "Ship Name",
+                    width: "190px"
+                }]
+            };
+        }
+    }
+
+})();
